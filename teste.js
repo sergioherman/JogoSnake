54085f8f882090a328e8a9ef3019cb30c3e4d230
Sergio <sergiohermanbl@gmail.com>
2020-05-27 23:21:18 -0300

    Alterações Gerais

--- teste.js
+++ teste.js
@@ -1,43 +1,20 @@
-// ii=0
-// anda()
-// function anda(){
-//     console.log('oi')
-//     var timeout1 = setTimeout(anda,1000)
-//     ii++
-//     n=[]
-//     n.push(1)
-//     console.log(ii)
-//     if (ii>3){
-//     clearTimeout(timeout1)
-//     }
-// }
+declaravar()
+run()
 
-continua = true
-anda()
-function anda () {
-    var data = new Date();
-    var mmsinicio = data.getMilliseconds();
-    console.log(mmsinicio)
+function declaravar(){
+    nn = 0
+    tempo = 500
+}
+function run(){
+    nn++
+    temporizador = setTimeout(run,tempo)
+    funcaolog()
+}
 
-    if (continua == false){
-        clearTimeout(timeout1)
-    }else{
-        var timeout1 = setTimeout(anda,1000)
+function funcaolog(){
+    console.log(nn)
+    if (nn == 3){
+        console.log('Hello World')
+        clearTimeout(temporizador)
     }
-        
-    var data = new Date();
-    var mmsfim = data.getMilliseconds();
-    document.getElementById('fim').value=mmsfim
-    if (bateu == false && continua == true) {
-        if(mmsinicio <= mmsfim){
-            var tempopr = mmsfim - mmsinicio
-            // if (bateu == false){
-            document.getElementById('tempoproc').value+=tempopr + ' - '
-            // }
-        }else {
-            document.getElementById('tempoproc').value+= 'min-over'
-        }
-
-    }
-// ----------------------------------------------------
-
+}
